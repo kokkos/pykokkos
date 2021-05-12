d08ab9c300ecf8dc8f5c918c937f9223eaeb7b6a
Nader Al Awar <naderalawar@gmail.com>
2021-05-12 18:01:35 -0500

    KokkosfunctionVisitor: allow atomic functions in kokkos functions

--- pykokkos/core/visitors/kokkosfunction_visitor.py
+++ pykokkos/core/visitors/kokkosfunction_visitor.py
@@ -1,4 +1,5 @@
 import ast
+import re
 from typing import List
 
 from pykokkos.core import cppast
@@ -31,6 +32,38 @@ class KokkosFunctionVisitor(PyKokkosVisitor):
         method.is_const = True
 
         return method
+
+    def visit_Call(self, node: ast.Call) -> cppast.CallExpr:
+        # Copied from workunit_visitor.py
+        name: str = visitors_util.get_node_name(node.func)
+        args: List[cppast.Expr] = [self.visit(a) for a in node.args]
+
+        function = cppast.DeclRefExpr(f"Kokkos::{name}")
+
+        atomic_fetch_op: re.Pattern = re.compile("atomic_fetch_*")
+        is_atomic_fetch_op: bool = atomic_fetch_op.match(name)
+        is_atomic_compare_exchange: bool = name == "atomic_compare_exchange"
+
+        if is_atomic_fetch_op or is_atomic_compare_exchange:
+            if is_atomic_fetch_op and len(args) != 3:
+                self.error(node, "atomic_fetch_op functions take exactly 3 arguments")
+            if is_atomic_compare_exchange and len(args) != 4:
+                self.error(node, "atomic_compare_exchange takes exactly 4 arguments")
+
+            # convert indices
+            args[0] = cppast.CallExpr(args[0], args[1].exprs)
+            del args[1]
+
+            # if not isinstance(args[0], cppast.CallExpr):
+            #     self.error(
+            #         node, "atomic_fetch_op functions only support views")
+
+            # atomic_fetch_* operations need to have an address as
+            # their first argument
+            args[0] = cppast.UnaryOperator(args[0], cppast.BinaryOperatorKind.AddrOf)
+            return cppast.CallExpr(function, args)
+
+        return super().visit_Call(node)
 
     # Checks that a function marked as kokkos_function
     # is annotated with a return type if it returns
