60256639e4b59b5376f3bdb8a82fa76429682709
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-03-23 10:21:30 -0600

    MAINT: PR 10 revisions
    * order the argument of `assert_allclose()` to match
    the docs

--- tests/test_kokkosfunctions_translator.py
+++ tests/test_kokkosfunctions_translator.py
@@ -166,7 +166,7 @@ class TestKokkosFunctionsTranslator(unittest.TestCase):
         expected_result: float = self.threads * (self.f_1 + self.f_2)
         result: float = pk.parallel_reduce(self.range_policy, self.functor.add_floats)
 
-        assert_allclose(expected_result, result)
+        assert_allclose(result, expected_result)
 
     def test_nested_sum(self):
         expected_result: int = self.threads * (self.i_1 + self.i_2)
