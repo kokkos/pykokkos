169d5903f05c84a7a9312c109d154ed628edba46
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-05-12 16:50:15 -0600

    MAINT: PR 29 revisions
    * switch to simpler workunits for the reciprocal
    ufunc implementation, and use `functools.singledispatch`
    for argument type dispatching
    
    * unfortunately, at the moment, it looks like `pykokkos`
    view type annotations are not suitable for `singledispatch`
    because they are not not formally Python classes;
    generic classes are mostly incompatible with runtime checks
    I think per: https://stackoverflow.com/a/58445388/2942522
    
    ```
    _________________________________________________ ERROR collecting tests/test_ufuncs.py _________________________________________________
    tests/test_ufuncs.py:1: in <module>
        import pykokkos as pk
    pykokkos/__init__.py:15: in <module>
        from pykokkos.lib.ufuncs import reciprocal # type: ignore
    pykokkos/lib/ufuncs.py:49: in <module>
        def _(view: pk.View1D[pk.double]):
    /usr/lib/python3.10/functools.py:873: in register
        raise TypeError(
    E   TypeError: Invalid annotation for 'view'. pykokkos.interface.views.View1D[pykokkos.interface.data_types.double] is not a class.
    ```
    
    - pretty sure that is a valid `view` type annotation right? this may
    require some plumbing changes?

--- pykokkos/lib/ufuncs.py
+++ pykokkos/lib/ufuncs.py
@@ -1,33 +1,28 @@
+from functools import singledispatch
+
 import pykokkos as pk
 
 # NOTE: it might be slick if we could encapsulate
-# the workload code inside of the ufunc definitions,
+# the workunit code inside of the ufunc definitions,
 # but we currently require global scoping
 
 
-@pk.workload
-class ReciprocalView1D:
-    def __init__(self, view: pk.View1D[pk.double]):
-        # TODO: why doesn't view have a "size"
-        # attribute? it is basically the shape product
-        # in higher dims
-        self.threads: int = view.shape[0] # type: ignore
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def reciprocal_impl_1d_double(view: pk.View1D[pk.double], tid: int):
+    view[tid] = 1 / view[tid] # type: ignore
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
+@pk.workunit
+def reciprocal_impl_1d_single(view: pk.View1D[pk.single], tid: int):
+    view[tid] = 1 / view[tid] # type: ignore
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = 1 / self.view[tid] # type: ignore
 
 
 # TODO: how are we going to "ufunc dispatch" when the view
 # has a different type/precision? i.e., something other than
 # pk.double?
 
-def reciprocal(view: pk.View1D[pk.double]):
+@singledispatch
+def reciprocal(view):
     """
     Return the reciprocal of the argument, element-wise.
 
@@ -47,15 +42,16 @@ def reciprocal(view: pk.View1D[pk.double]):
         This function is not designed to work with integers.
 
     """
-    workload_instance = ReciprocalView1D(view=view)
-    # NOTE: should our ufuncs have an option to allow
-    # CUDA vs. OpenMP execution directly in their call
-    # signature?
-    pk.execute(pk.ExecutionSpace.Default, workload_instance)
-    # TODO: how should we design this?
-    # it seems a bit problematic to both mutate the data
-    # underneath a view in-place and return a reference to the view?
-    # returning a new view/"array" would be more similar to
-    # NumPy?
-    return workload_instance.view
+    pass
+
+
+@reciprocal.register
+def _(view: pk.View1D[pk.double]):
+    pk.parallel_for(view.shape[0], reciprocal_impl_1d_double, view=view)
+    return view
+
+@reciprocal.register
+def _(view: pk.View1D[pk.single]):
+    pk.parallel_for(view.shape[0], reciprocal_impl_1d_single, view=view)
+    return view
     
