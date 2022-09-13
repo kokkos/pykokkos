67646230378a1750677f27711aeac5c0525de849
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-09-13 10:29:38 -0600

    MAINT: PR 82 revisions
    * `*.sh` files were not getting installed so fix
    this in `setup.py` (was causing an array API test
    suite failure)

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="pykokkos",
     version="0.1",
     packages=find_packages(include=["pykokkos", "pykokkos.*"]),
-    include_package_data=True
+    include_package_data=True,
+    package_data={"": ["*.sh"]}
 )
