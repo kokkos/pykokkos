a4bc17e6db1948abc5028cbef0a310cf4ba4b062
UmairShahzad <18100099@lums.edu.pk>
2022-09-29 13:47:33 -0500

    fixed tests: removed is_hashable and added utils to mypy ignore list

--- pykokkos/interface/views.py
+++ pykokkos/interface/views.py
@@ -602,21 +602,13 @@ def from_cupy(array) -> ViewType:
     return from_numpy(np_array, MemorySpace.CudaSpace, layout)
 
 
-def is_hashable(obj):
-    try:
-        hash(obj)
-    except TypeError:
-        return False
-    return True
-
-
 # asarray is required for comformance with the array API:
 # https://data-apis.org/array-api/2021.12/API_specification/creation_functions.html#objects-in-api
 def asarray(obj, /, *, dtype=None, device=None, copy=None):
     # TODO: proper implementation/design
     # for now, let's cheat and use NumPy asarray() followed
     # by pykokkos from_numpy()
-    if is_hashable(obj) and obj in {pk.e, pk.pi, pk.inf, pk.nan}:
+    if obj in {pk.e, pk.pi, pk.inf, pk.nan}:
         if dtype is None:
             dtype = pk.float64
         view = pk.View([1], dtype=dtype)
