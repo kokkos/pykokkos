61797c3a4aef5f1b4dc725eb1ea6840ea3a025d4
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-10-31 11:49:41 -0600

    ENH: cos to API std()
    * adjust `cos()` ufunc such that is passes the array API
    standard test, and turn that test on in CI

--- pykokkos/lib/ufuncs.py
+++ pykokkos/lib/ufuncs.py
@@ -1724,8 +1724,20 @@ def cos_impl_1d_double(tid: int, view: pk.View1D[pk.double], out: pk.View1D[pk.d
 
 
 @pk.workunit
+def cos_impl_2d_double(tid: int, view: pk.View2D[pk.double], out: pk.View2D[pk.double]):
+    for i in range(view.extent(1)):
+        out[tid][i] = cos(view[tid][i])
+
+
+@pk.workunit
 def cos_impl_1d_float(tid: int, view: pk.View1D[pk.float], out: pk.View1D[pk.float]):
     out[tid] = cos(view[tid])
+
+
+@pk.workunit
+def cos_impl_2d_float(tid: int, view: pk.View2D[pk.float], out: pk.View2D[pk.float]):
+    for i in range(view.extent(1)):
+        out[tid][i] = cos(view[tid][i])
 
 
 def cos(view):
@@ -1743,14 +1755,20 @@ def cos(view):
            Output view.
 
     """
-    if len(view.shape) > 1:
-        raise NotImplementedError("only 1D views currently supported for cos() ufunc.")
-    if str(view.dtype) == "DataType.double":
-        out = pk.View([view.shape[0]], pk.double)
-        pk.parallel_for(view.shape[0], cos_impl_1d_double, view=view, out=out)
-    elif str(view.dtype) == "DataType.float":
-        out = pk.View([view.shape[0]], pk.float)
-        pk.parallel_for(view.shape[0], cos_impl_1d_float, view=view, out=out)
+    if len(view.shape) > 2:
+        raise NotImplementedError("only up to 2D views currently supported for cos() ufunc.")
+    if "double" in str(view.dtype) or "float64" in str(view.dtype):
+        out = pk.View([*view.shape], dtype=pk.float64)
+        if len(view.shape) == 1:
+            pk.parallel_for(view.shape[0], cos_impl_1d_double, view=view, out=out)
+        elif len(view.shape) == 2:
+            pk.parallel_for(view.shape[0], cos_impl_2d_double, view=view, out=out)
+    elif "float" in str(view.dtype):
+        out = pk.View([*view.shape], dtype=pk.float32)
+        if len(view.shape) == 1:
+            pk.parallel_for(view.shape[0], cos_impl_1d_float, view=view, out=out)
+        elif len(view.shape) == 2:
+            pk.parallel_for(view.shape[0], cos_impl_2d_float, view=view, out=out)
     else:
         raise NotImplementedError
     return out
