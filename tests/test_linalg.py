b9e2eb34c96590ee438c95e06512bf9615abfebc
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-12-12 17:31:36 -0700

    ENH: DGEMM non-optimized
    * get some initial regression tests in place by adding
    a non-optimized DGEMM algorithm--specifically `Algorithm 1`
    from Kurzak et al. (2012)
    
    * my main objective here is just to have some tests in place
    before we try to move on to more advanced algorithms or perhaps
    even autotuning? Not sure how far down that rabbit hole we
    plan to go just yet, and which knobs we can reasonably expect
    to turn from PyKokkos alone

---
+++ tests/test_linalg.py
@@ -0,0 +1,100 @@
+import pykokkos as pk
+from pykokkos.linalg.l3_blas import dgemm
+
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+
+
+@pytest.mark.parametrize("shape_a, shape_b", [
+    ([3, 3], [3, 3]),
+    ([11, 7], [7, 5]),
+])
+def test_dgemm_shape(shape_a, shape_b):
+    view_a = pk.View(shape_a, dtype=pk.double)
+    view_b = pk.View(shape_b, dtype=pk.double)
+    alpha = 1.0
+    actual = dgemm(alpha=alpha,
+                   view_a=view_a,
+                   view_b=view_b)
+    # shape requirement of C
+    assert actual.shape == (view_a.shape[0], view_b.shape[1])
+
+
+@pytest.mark.parametrize("alpha, a, b, beta, expected_c", [
+    (1.0,
+     np.array([[1, 7, 9],
+               [3, 1, 3],
+               [5, 5, 22]], dtype=np.float64),
+     np.array([[9, 0, 2],
+               [77, 100, 4],
+               [1, 500, 9]], dtype=np.float64),
+     1.0,
+     np.array([[557.,  5200., 111.],
+               [107.,  1600., 37.],
+               [452., 11500., 228.]], dtype=np.float64),
+     ),
+    (7.7,
+     np.array([[8, 7, 1, 200, 55.3],
+               [99.2, 1.11, 2.02, 17.7, 900.2],
+               [5.01, 15.21, 22.07, 1.09, 22.22],
+               [1, 2, 3, 4, 5]], dtype=np.float64),
+     np.array([[9, 0, 2],
+               [77, 100, 4],
+               [1, 500, 9],
+               [226.68, 11.61, 12.12],
+               [17.7, 200.10, 301.17]], dtype=np.float64),
+     1.0,
+     np.array([[361336.437, 112323.981, 147314.0977],
+               [161130.7082, 1397215.1809, 2090925.5906],
+               [14466.03004, 131014.55213, 53705.17614],
+               [8941.394, 21151.438, 12253.241]], dtype=np.float64),
+     ),
+    # this case just expands b by one column from
+    # case above
+    (7.7,
+     np.array([[8, 7, 1, 200, 55.3],
+               [99.2, 1.11, 2.02, 17.7, 900.2],
+               [5.01, 15.21, 22.07, 1.09, 22.22],
+               [1, 2, 3, 4, 5]], dtype=np.float64),
+     np.array([[9, 0, 2, 19],
+               [77, 100, 4, 19],
+               [1, 500, 9, 19],
+               [226.68, 11.61, 12.12, 19],
+               [17.7, 200.10, 301.17, 20]], dtype=np.float64),
+     1.0,
+     np.array([[361336.437, 112323.981, 147314.0977, 40117.],
+               [161130.7082, 1397215.1809, 2090925.5906, 156191.189],
+               [14466.03004, 131014.55213, 53705.17614, 9768.374],
+               [8941.394, 21151.438, 12253.241, 2233.]], dtype=np.float64),
+     ),
+])
+def test_dgemm_vs_scipy(alpha,
+                        a,
+                        b,
+                        beta,
+                        expected_c):
+    # test against expected results from
+    # scipy.linalg.blas.dgemm
+    # NOTE: it seems that SciPy may ignore beta
+    # in its wrapper perhaps?
+    # see: https://github.com/scipy/scipy/issues/17594
+    # so I've kept beta fixed at 1.0 for now in these
+    # tests
+    view_a = pk.from_numpy(a)
+    view_b = pk.from_numpy(b)
+    actual_c = dgemm(alpha=alpha,
+                     view_a=view_a,
+                     view_b=view_b,
+                     beta=beta)
+    assert_allclose(actual_c, expected_c)
+
+
+def test_dgemm_input_handling():
+    alpha = 1.0
+    view_a = pk.from_numpy(np.zeros((4, 3)))
+    view_b = pk.from_numpy(np.zeros((4, 3)))
+    with pytest.raises(ValueError, match="Second dimensions"):
+        dgemm(alpha=alpha,
+              view_a=view_a,
+              view_b=view_b)
