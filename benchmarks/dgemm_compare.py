1cdbb9c57a1755440c023333aede3350d688101b
Tyler Reddy <tyler.je.reddy@gmail.com>
2023-01-05 17:07:06 -0700

    BENCH: fixup benchmarks
    * remove `threadpoolctl` stuff and switch to using
    `OMP_NUM_THREADS` manually + do way more trials
    and use boxplots to better visualize outliers I might
    be concerned about

--- benchmarks/dgemm_compare.py
+++ benchmarks/dgemm_compare.py
@@ -3,7 +3,8 @@ Compare DGEMM performance with SciPy
 (i.e., a wheel with OpenBLAS 0.3.18)
 """
 
-from threadpoolctl import threadpool_limits
+import os
+
 import pykokkos as pk
 from pykokkos.linalg.l3_blas import dgemm as pk_dgemm
 
@@ -13,31 +14,43 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 
 if __name__ == "__main__":
     import timeit
-    for num_threads in [1, 4, 10]:
-        print("num OpenMP threads:", num_threads)
-        num_repeats = 50
-        results = {"PyKokkos": {},
-                   "SciPy": {}}
-        alpha, a, b, c, beta = (3.6,
-                                np.array([[8, 7, 1, 200, 55.3],
-                                          [99.2, 1.11, 2.02, 17.7, 900.2],
-                                          [5.01, 15.21, 22.07, 1.09, 22.22],
-                                          [1, 2, 3, 4, 5]], dtype=np.float64),
-                                np.array([[9, 0, 2, 19],
-                                          [77, 100, 4, 19],
-                                          [1, 500, 9, 19],
-                                          [226.68, 11.61, 12.12, 19],
-                                          [17.7, 200.10, 301.17, 20]], dtype=np.float64),
-                                np.ones((4, 4)) * 3.3,
-                                4.3)
-        for system_size in ["small", "medium", "large"]:
-            print("-" * 20)
-            print(f"system size: {system_size}")
+    num_global_repeats = 50
+    num_repeats = 5000
+    results = {
+               "PyKokkos": {"small": [],
+                            "medium": [],
+                            "large": []},
+               "SciPy": {"small": [],
+                         "medium": [],
+                         "large": []},
+               }
+    alpha, a, b, c, beta = (3.6,
+                            np.array([[8, 7, 1, 200, 55.3],
+                                      [99.2, 1.11, 2.02, 17.7, 900.2],
+                                      [5.01, 15.21, 22.07, 1.09, 22.22],
+                                      [1, 2, 3, 4, 5]], dtype=np.float64),
+                            np.array([[9, 0, 2, 19],
+                                      [77, 100, 4, 19],
+                                      [1, 500, 9, 19],
+                                      [226.68, 11.61, 12.12, 19],
+                                      [17.7, 200.10, 301.17, 20]], dtype=np.float64),
+                            np.ones((4, 4)) * 3.3,
+                            4.3)
+    num_threads = os.environ.get("OMP_NUM_THREADS")
+    df = pd.DataFrame(np.full(shape=(num_global_repeats * 2, 4), fill_value=np.nan),
+                      columns=["backend", "small", "medium", "large"])
+    df["backend"] = df["backend"].astype(str)
+    if num_threads is None:
+        raise ValueError("must set OMP_NUM_THREADS for benchmarks!")
 
+    counter = 0
+    for global_repeat in tqdm(range(1, num_global_repeats + 1)):
+        for col_num, system_size in tqdm(enumerate(["small", "medium", "large"]), total=3):
             if system_size == "medium":
                 a_new = np.tile(a, (10, 1))
                 b_new = np.tile(b, (1, 10))
@@ -54,32 +67,28 @@ if __name__ == "__main__":
             view_a = pk.from_numpy(a_new)
             view_b = pk.from_numpy(b_new)
             view_c = pk.from_numpy(c_new)
-            with threadpool_limits(limits=num_threads, user_api='openmp'):
-                pk_dgemm_time_sec = timeit.timeit("pk_dgemm(alpha, view_a, view_b, beta, view_c)",
-                                                  globals=globals(),
-                                                  number=num_repeats)
-            results["PyKokkos"][system_size] = pk_dgemm_time_sec
-            print(f"PyKokkos DGEMM execution time (s) for {num_repeats} repeats: {pk_dgemm_time_sec}")
+            pk_dgemm_time_sec = timeit.timeit("pk_dgemm(alpha, view_a, view_b, beta, view_c)",
+                                              globals=globals(),
+                                              number=num_repeats)
+            results["PyKokkos"][system_size].append(pk_dgemm_time_sec)
+            df.iloc[counter, 0] = "PyKokkos"
+            df.iloc[counter, col_num + 1] = pk_dgemm_time_sec
             scipy_dgemm_time_sec = timeit.timeit("scipy_dgemm(alpha, a_new, b_new, beta, c_new)",
                                                  globals=globals(),
                                                  number=num_repeats)
-            results["SciPy"][system_size] = scipy_dgemm_time_sec
-            print(f"SciPy DGEMM execution time (s) for {num_repeats} repeats: {scipy_dgemm_time_sec}")
-            ratio = pk_dgemm_time_sec / scipy_dgemm_time_sec
-            if ratio == 1:
-                print("PyKokkos DGEMM timing is identical to SciPy")
-            elif ratio > 1:
-                print(f"PyKokkos DGEMM timing is slower than SciPy with ratio: {ratio:.2f} fold")
-            else:
-                print(f"PyKokkos DGEMM timing is faster than SciPy with ratio: {ratio:.2f} fold")
-            print("-" * 20)
-        df = pd.DataFrame.from_dict(results)
-        print("df:\n", df)
-        fig, ax = plt.subplots()
-        df.plot.bar(ax=ax,
-                    rot=0,
-                    logy=True,
-                    xlabel="Problem Size",
-                    ylabel=f"log of time (s) for {num_repeats} repeats",
-                    title="DGEMM Performance Comparison with timeit")
-        fig.savefig(f"DGEMM_perf_compare_{num_threads}_threads.png", dpi=300)
+            results["SciPy"][system_size].append(scipy_dgemm_time_sec)
+            df.iloc[counter + 1, 0] = "SciPy"
+            df.iloc[counter + 1, col_num + 1] = scipy_dgemm_time_sec
+        counter += 2
+
+    print("df:\n", df)
+    fig, axes = plt.subplots(nrows=1, ncols=3)
+    fig.set_size_inches(12, 5)
+    df.boxplot(ax=axes,
+               by="backend",
+               )
+    for ax in axes:
+        ax.set_xlabel("Backend")
+    axes[0].set_ylabel(f"Time (s) for {num_repeats} DGEMM executions")
+    fig.suptitle(f"DGEMM performance boxplots (OMP_NUM_THREADS={num_threads}; {num_global_repeats} trials) for different problem sizes")
+    fig.savefig(f"DGEMM_perf_compare_{num_threads}_threads.png", dpi=300)
