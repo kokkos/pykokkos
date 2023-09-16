8790826ac64709b497d2fce8f022b554c1db12e2
HannanNaeem <hannan.2455@gmail.com>
2023-09-15 19:15:00 -0500

    basic type inference for policy params

--- examples/kokkos-tutorials/standalone/02_n.py
+++ examples/kokkos-tutorials/standalone/02_n.py
@@ -5,19 +5,10 @@ import pykokkos as pk
 from parse_args import parse_args
 
 @pk.workunit
-def y_init(i, y: pk.View1D[pk.double]):
-    y[i] = 1
-
-@pk.workunit
-def matrix_init(j, M: int, A: pk.View1D[pk.double]):
-    for i in range(M):
-        A[j * M + i] = 1
-
-@pk.workunit
-def yAx(j:int, acc: pk.Acc[float], M: int, y: pk.View1D[pk.double], x: pk.View1D[pk.double], A: pk.View1D[pk.double]):
+def yAx(j: int, acc: pk.Acc[float], M: int, y: pk.View1D[pk.double], x: pk.View1D[pk.double], A: pk.View2D[pk.double]):
     temp2: float = 0
     for i in range(M):
-        temp2 += A[j * M + i] * x[i]
+        temp2 += A[j][i] * x[i]
 
     acc += y[j] * temp2
 
@@ -25,17 +16,30 @@ def run() -> None:
     values: Tuple[int, int, int, int, int, bool] = parse_args()
     N: int = values[0]
     M: int = values[1]
-    nrepeat: int = 1 
+    fill: bool = values[-1]
+    nrepeat: int = 100
     print(f"Total size S = {N * M} N = {N} M = {M}")
 
-    y = pk.View([N], pk.double)
-    x = pk.View([M], pk.double)
-    A = pk.View([N * M], pk.double)
+    y: pk.View1D = pk.View([N], pk.double)
+    x: pk.View1D = pk.View([M], pk.double)
+    A: pk.View2D = pk.View([N, M], pk.double)
+
+    if fill:
+        y.fill(1)
+        x.fill(1)
+        A.fill(1)
+    else:
+        for i in range(N):
+            y[i] = 1
+
+        for i in range(M):
+            x[i] = 1
+
+        for j in range(N):
+            for i in range(M):
+                A[j][i] = 1
 
     p = pk.RangePolicy(pk.get_default_space(), 0, N)
-    pk.parallel_for(p, y_init, y=y)
-    pk.parallel_for(pk.RangePolicy(pk.get_default_space(), 0, M), y_init, y=x)
-    pk.parallel_for(p, matrix_init, M=M, A=A)
 
     timer = pk.Timer()
 
@@ -45,10 +49,11 @@ def run() -> None:
     timer_result = timer.seconds()
 
     print(f"Computed result for {N} x {M} is {result}")
-    solution = N * M
+    solution: float = N * M
 
     if result != solution:
-        pk.printf("Error: result (%lf) != solution (%lf)\n", result, solution)
+        pk.printf("Error: result (%lf) != solution (%lf)\n",
+                  result, solution)
 
     print(f"N({N}) M({M}) nrepeat({nrepeat}) problem(MB) time({timer_result}) bandwidth(GB/s)")
 
