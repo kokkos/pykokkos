d9a9c98d3378da489c66d8d87d3c0e294318f8fd
Nader Al Awar <naderalawar@gmail.com>
2023-10-25 14:14:56 -0500

    ExecutionSpace: create new ExecutionSpaceInstance class to hold cupy streams and rename Pthreads to Threads

--- pykokkos/interface/execution_space.py
+++ pykokkos/interface/execution_space.py
@@ -6,7 +6,7 @@ class ExecutionSpace(Enum):
     Cuda = "Cuda"
     HIP = "HIP"
     OpenMP = "OpenMP"
-    Pthreads = "Pthreads"
+    Threads = "Threads"
     Serial = "Serial"
     Debug = "Debug"
     Default = "Default"
@@ -22,4 +22,38 @@ def is_host_execution_space(space: ExecutionSpace) -> bool:
     if space is ExecutionSpace.Default:
         space = km.get_default_space()
 
-    return space in {ExecutionSpace.OpenMP, ExecutionSpace.Pthreads, ExecutionSpace.Serial}
+    return space in {ExecutionSpace.OpenMP, ExecutionSpace.Threads, ExecutionSpace.Serial}
+
+class ExecutionSpaceInstance:
+    """
+    An instance of the execution space, corresponding to Cuda/HIP
+    streams
+    """
+
+    def __init__(self, space: ExecutionSpace, stream = None):
+        """
+        ExecutionSpaceInstance constructor
+
+        :param space: the exectuion space requested
+        :param stream: optional stream argument, only valid for Cuda and HIP
+        """
+
+        if space is ExecutionSpace.Default:
+            space = km.get_default_space()
+
+        self.space: ExecutionSpace = space
+
+        instance_constructor = getattr(km.get_kokkos_module(is_host_execution_space(space)), f"KokkosExecutionSpace_{space.value}")
+
+        if stream is not None:
+            if space.value not in {"Cuda", "HIP"}:
+                raise ValueError(f"Stream argument unsupported for space {space}")
+
+            import cupy as cp
+
+            if not isinstance(stream, cp.cuda.Stream):
+                raise TypeError(f"Type {type(stream)} unsupported; Only CuPy streams allowed")
+
+            self.instance = instance_constructor(stream.ptr)
+        else:
+            self.instance = instance_constructor()
