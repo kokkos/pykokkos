ab81bafefc5d1641c597fd980e074eb5388fbc9d
kennykos <gkk345@c301-003.ls6.tacc.utexas.edu>
2024-12-18 11:27:43 -0600

    Interface: add support for Kokkos Mathematical Special Functions
    Special functions defined in https://github.com/kokkos/kokkos/blob/9fa2a01747f0a30ca2723c8d7d0a22c95a05717a/core/src/Kokkos_MathematicalSpecialFunctions.hpp#L445
    
    List of special functions:
    * expint1
    * erfcx
    * cyl_bessel_j0
    * cyl_bessel_y0
    * cyl_bessel_i0
    * cyl_bessel_k0
    * cyl_bessel_j1
    * cyl_bessel_y1
    * cyl_bessel_i1
    * cyl_bessel_k1
    * cyl_bessel_h10
    * cyl_bessel_h11
    * cyl_bessel_h20
    * cyl_bessel_h21

--- pykokkos/interface/mathematical_special_functions.py
+++ pykokkos/interface/mathematical_special_functions.py
@@ -1,6 +1,41 @@
+def expint1(input: float) -> float:
+    pass
+
+def erfc(input: float) -> float:
+    pass
 
 def cyl_bessel_j0(input: float) -> float:
     pass
 
+def cyl_bessel_y0(input: float) -> float:
+    pass
+
+def cyl_bessel_i0(input: float) -> float:
+    pass
+
+def cyl_bessel_k0(input: float) -> float:
+    pass
+
 def cyl_bessel_j1(input: float) -> float:
+    pass
+
+def cyl_bessel_y1(input: float) -> float:
+    pass
+
+def cyl_bessel_i1(input: float) -> float:
+    pass
+
+def cyl_bessel_k1(input: float) -> float:
+    pass
+
+def cyl_bessel_h10(input: float) -> float:
+    pass
+
+def cyl_bessel_h11(input: float) -> float:
+    pass
+
+def cyl_bessel_h20(input: float) -> float:
+    pass
+
+def cyl_bessel_h21(input: float) -> float:
     pass
