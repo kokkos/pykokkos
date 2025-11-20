380c953d809a7b37b06faa7d9ab9cc59a59c8a15
Ivan Grigorik <givan502@gmail.com>
2025-11-20 20:14:52 +0000

    formatting

--- pykokkos/interface/interface_util.py
+++ pykokkos/interface/interface_util.py
@@ -1,17 +1,19 @@
 import sys
 
+
 def get_lineno(frame):
     """
     Return current line number `inspect` frame
     """
     return frame.f_lineno
 
-def get_filename(frame):
 
+def get_filename(frame):
     """
     Return current line number `inspect` frame
     """
     return frame.f_code.co_filename
+
 
 def generic_error(filename: str, lineno: str | int, error: str, exit_message: str):
     print(f"\n\033[31m\033[01mError {filename}:{lineno}\033[0m: {error}")
