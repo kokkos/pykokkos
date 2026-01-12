d96b11d7f586caed344848b8c53682bd507c13a0
Ivan Grigorik <givan502@gmail.com>
2026-01-12 22:32:50 +0000

    deprecate workloads for `test_ufuncs`

--- tests/test_ufuncs.py
+++ tests/test_ufuncs.py
@@ -6,310 +6,130 @@ from numpy.testing import assert_allclose
 import pytest
 
 
-@pk.workload
-class SqrtView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def sqrt_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = sqrt(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = sqrt(self.view[tid])
+@pk.workunit
+def exp_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = exp(view[tid])
 
 
-@pk.workload
-class ExpView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def exp2_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = exp2(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = exp(self.view[tid])
+@pk.workunit
+def positive_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = positive(view[tid])
 
 
-@pk.workload
-class Exp2View1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def negative_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = negative(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = exp2(self.view[tid])
+@pk.workunit
+def absolute_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = abs(view[tid])
 
 
-@pk.workload
-class PositiveView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def fabsolute_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = fabs(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = positive(self.view[tid])
+@pk.workunit
+def rint_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = rint(view[tid])
 
 
-@pk.workload
-class NegativeView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def conjugate_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = conj(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = negative(self.view[tid])
+@pk.workunit
+def sign_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = sign(view[tid])
 
 
-@pk.workload
-class AbsoluteView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def log_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = log(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = abs(self.view[tid])
+@pk.workunit
+def log2_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = log2(view[tid])
 
 
-@pk.workload
-class FabsoluteView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def log10_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = log10(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = fabs(self.view[tid])
+@pk.workunit
+def expm1_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = expm1(view[tid])
 
 
-@pk.workload
-class RintView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def log1p_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = log1p(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = rint(self.view[tid])
+@pk.workunit
+def square_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = square(view[tid])
 
 
-@pk.workload
-class ConjugateView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
+@pk.workunit
+def cbrt_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = cbrt(view[tid])
 
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
 
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = conj(self.view[tid])
-
-
-@pk.workload
-class SignView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = sign(self.view[tid])
-
-
-@pk.workload
-class LogView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = log(self.view[tid])
-
-
-@pk.workload
-class Log2View1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = log2(self.view[tid])
-
-
-@pk.workload
-class Log10View1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = log10(self.view[tid])
-
-
-@pk.workload
-class Expm1View1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = expm1(self.view[tid])
-
-
-@pk.workload
-class Log1pView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = log1p(self.view[tid])
-
-
-@pk.workload
-class SquareView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = square(self.view[tid])
-
-
-@pk.workload
-class CbrtView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = cbrt(self.view[tid])
-
-
-@pk.workload
-class ReciprocalView1D:
-    def __init__(self, threads: int, view: pk.View1D[pk.double]):
-        self.threads: int = threads
-        self.view: pk.View1D[pk.double] = view
-
-    @pk.main
-    def run(self) -> None:
-        pk.parallel_for(self.threads, self.pfor)
-
-    @pk.workunit
-    def pfor(self, tid: int) -> None:
-        self.view[tid] = reciprocal(self.view[tid])
+@pk.workunit
+def reciprocal_workunit(tid: int, view: pk.View1D[pk.double]) -> None:
+    view[tid] = reciprocal(view[tid])
 
 
 @pytest.mark.parametrize(
-    "kokkos_test_class, numpy_ufunc",
+    "kokkos_workunit, numpy_ufunc",
     [
-        (SqrtView1D, np.sqrt),
-        (ExpView1D, np.exp),
-        pytest.param(Exp2View1D, np.exp2, marks=pytest.mark.xfail(reason="see gh-27")),
+        (sqrt_workunit, np.sqrt),
+        (exp_workunit, np.exp),
+        pytest.param(exp2_workunit, np.exp2, marks=pytest.mark.xfail(reason="see gh-27")),
         pytest.param(
-            PositiveView1D, np.positive, marks=pytest.mark.xfail(reason="see gh-27")
+            positive_workunit, np.positive, marks=pytest.mark.xfail(reason="see gh-27")
         ),
         pytest.param(
-            NegativeView1D, np.negative, marks=pytest.mark.xfail(reason="see gh-27")
+            negative_workunit, np.negative, marks=pytest.mark.xfail(reason="see gh-27")
         ),
-        (AbsoluteView1D, np.absolute),
-        (FabsoluteView1D, np.fabs),
-        pytest.param(RintView1D, np.rint, marks=pytest.mark.xfail(reason="see gh-27")),
+        (absolute_workunit, np.absolute),
+        (fabsolute_workunit, np.fabs),
+        pytest.param(rint_workunit, np.rint, marks=pytest.mark.xfail(reason="see gh-27")),
         pytest.param(
-            ConjugateView1D, np.conjugate, marks=pytest.mark.xfail(reason="see gh-27")
+            conjugate_workunit, np.conjugate, marks=pytest.mark.xfail(reason="see gh-27")
         ),
-        pytest.param(SignView1D, np.sign, marks=pytest.mark.xfail(reason="see gh-27")),
-        (LogView1D, np.log),
-        (Log2View1D, np.log2),
-        (Log10View1D, np.log10),
-        (Expm1View1D, np.expm1),
-        (Log1pView1D, np.log1p),
+        pytest.param(sign_workunit, np.sign, marks=pytest.mark.xfail(reason="see gh-27")),
+        (log_workunit, np.log),
+        (log2_workunit, np.log2),
+        (log10_workunit, np.log10),
+        (expm1_workunit, np.expm1),
+        (log1p_workunit, np.log1p),
         pytest.param(
-            SquareView1D, np.square, marks=pytest.mark.xfail(reason="see gh-27")
+            square_workunit, np.square, marks=pytest.mark.xfail(reason="see gh-27")
         ),
-        pytest.param(CbrtView1D, np.cbrt, marks=pytest.mark.xfail(reason="see gh-27")),
+        pytest.param(cbrt_workunit, np.cbrt, marks=pytest.mark.xfail(reason="see gh-27")),
         pytest.param(
-            ReciprocalView1D, np.reciprocal, marks=pytest.mark.xfail(reason="see gh-27")
+            reciprocal_workunit, np.reciprocal, marks=pytest.mark.xfail(reason="see gh-27")
         ),
     ],
 )
-def test_1d_unary_ufunc_vs_numpy(kokkos_test_class, numpy_ufunc):
+def test_1d_unary_ufunc_vs_numpy(kokkos_workunit, numpy_ufunc):
     # verify that we can easily recreate the functionality
     # of most NumPy "unary" ufuncs on 1D views/arrays without much
     # custom code
@@ -317,9 +137,8 @@ def test_1d_unary_ufunc_vs_numpy(kokkos_test_class, numpy_ufunc):
     # "pykokkos ufuncs" some day?
     view: pk.View1d = pk.View([10], pk.double)
     view[:] = np.arange(10, dtype=np.float64)
-    kokkos_instance = kokkos_test_class(threads=10, view=view)
-    pk.execute(pk.ExecutionSpace.Default, kokkos_instance)
-    actual = kokkos_instance.view
+    pk.parallel_for(10, kokkos_workunit, view=view)
+    actual = view
     expected = numpy_ufunc(range(10))
     assert_allclose(actual, expected)
 
