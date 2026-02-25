4e42b48b8d13ff9961bfec917477cc54f045f278
nayyirahsan <nayyirahsan@gmail.com>
2026-02-25 16:23:48 -0600

    format

--- pykokkos/__init__.py
+++ pykokkos/__init__.py
@@ -35,6 +35,7 @@ from pykokkos.interface.views import astype
 
 import numpy as np
 
+
 class PKArray(np.ndarray):
     def __new__(cls, array):
         return np.asarray(array).view(cls)
@@ -42,35 +43,39 @@ class PKArray(np.ndarray):
     @property
     def dtype(self):
         from pykokkos.interface import data_types as dt
+
         mapping = {
-            np.dtype('bool'):    dt.bool,
-            np.dtype('int8'):    dt.int8,
-            np.dtype('int16'):   dt.int16,
-            np.dtype('int32'):   dt.int32,
-            np.dtype('int64'):   dt.int64,
-            np.dtype('uint8'):   dt.uint8,
-            np.dtype('uint16'):  dt.uint16,
-            np.dtype('uint32'):  dt.uint32,
-            np.dtype('uint64'):  dt.uint64,
-            np.dtype('float32'): dt.float32,
-            np.dtype('float64'): dt.float64,
+            np.dtype("bool"): dt.bool,
+            np.dtype("int8"): dt.int8,
+            np.dtype("int16"): dt.int16,
+            np.dtype("int32"): dt.int32,
+            np.dtype("int64"): dt.int64,
+            np.dtype("uint8"): dt.uint8,
+            np.dtype("uint16"): dt.uint16,
+            np.dtype("uint32"): dt.uint32,
+            np.dtype("uint64"): dt.uint64,
+            np.dtype("float32"): dt.float32,
+            np.dtype("float64"): dt.float64,
         }
         return mapping.get(super().dtype, super().dtype)
+
 
 def _pk_func(np_func):
     def wrapper(*args, **kwargs):
         return PKArray(np_func(*args, **kwargs))
+
     return wrapper
 
-isnan    = _pk_func(np.isnan)
-isinf    = _pk_func(np.isinf)
+
+isnan = _pk_func(np.isnan)
+isinf = _pk_func(np.isinf)
 isfinite = _pk_func(np.isfinite)
-equal    = _pk_func(np.equal)
-sign     = _pk_func(np.sign)
-round    = _pk_func(np.round)
-trunc    = _pk_func(np.trunc)
-ceil     = _pk_func(np.ceil)
-floor    = _pk_func(np.floor)
+equal = _pk_func(np.equal)
+sign = _pk_func(np.sign)
+round = _pk_func(np.round)
+trunc = _pk_func(np.trunc)
+ceil = _pk_func(np.ceil)
+floor = _pk_func(np.floor)
 
 
 runtime_singleton.runtime = Runtime()
